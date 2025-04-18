4eaf19ae52be4bb85ef686888bf8cb616e61da6e
Anishkarlekar2002 <143108967+Anishkarlekar2002@users.noreply.github.com>
2025-04-18 13:12:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
 
